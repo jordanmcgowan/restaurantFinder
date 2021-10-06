1073c0e46331bc8f32697ab466afa69dff6b32d1
jordanmcgowan <jordan_mcgowan@yahoo.com>
2021-10-06 16:47:06 -0500

    Removes Flipper integration and updates README

--- app/src/main/java/com/finder/MyApp.kt
+++ app/src/main/java/com/finder/MyApp.kt
@@ -1,12 +1,6 @@
 package com.finder
 
 import android.app.Application
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
 import com.finder.di.DaggerPlacesComponent
 import com.finder.di.PlacesComponent
 import com.finder.di.PlacesManagerModule
@@ -21,16 +15,5 @@ class MyApp : Application() {
         super.onCreate()
         placesComponent =
             DaggerPlacesComponent.builder().placesManagerModule(PlacesManagerModule(this)).build()
-
-        SoLoader.init(this, false)
-
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-            val client = AndroidFlipperClient.getInstance(this)
-            val descriptorMapping = DescriptorMapping.withDefaults()
-            client.addPlugin(InspectorFlipperPlugin(this, descriptorMapping))
-            client.addPlugin(DatabasesFlipperPlugin(this))
-
-            client.start()
-        }
     }
 }
